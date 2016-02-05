4534db253983f7b2a19c1416a5bc10fa289320a0
CodeMaster7 <italianstalian_2@yahoo.com>
2016-02-05 06:57:27 -0700

    added more middleware body-parser added more libraries and connected to mongoDB

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var User = require('./models/user');
 
 var app = express();
 
-//Middleware
-app.use(morgan('dev'));
-
-app.get('/', function (req, res) {
-	var name = "Sam";
-	res.json("My name is " + name);
+mongoose.connect('mongodb://root:abc@ds059145.mongolab.com:59145/amazoneco', function (err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("Connected to the database");
+	}
 });
 
-app.get('/catname', function(req, res) {
-	res.json('batman');
-})
+//Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post('/create-user', function (req, res, next) {
+	var user = new User();
+	
+	user.profile.name = req.body.name;
+	user.password = req.body.password;
+	user.email = req.body.email;
+	
+	user.save(function (err) {
+		if (err) return next(err);
+		
+		res.json('Succesfully created a new user');
+		
+	});
+});
+
 
 app.listen(3000, function(err) {
 	if (err) throw err;
