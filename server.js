7d573534bcb7e0865618c382fab834c7cb810768
CodeMaster7 <italianstalian_2@yahoo.com>
2016-02-15 18:48:49 -0700

    created a config folder and added more routes an navbar and footer with logging inputs loging working

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
 
+var secret = require('./config/secret');
 var User = require('./models/user');
 
 var app = express();
 
-mongoose.connect('mongodb://root:abc@ds059145.mongolab.com:59145/amazoneco', function (err) {
+mongoose.connect(secret.database, function (err) {
 	if (err) {
 		console.log(err);
 	} else {
@@ -16,28 +22,31 @@ mongoose.connect('mongodb://root:abc@ds059145.mongolab.com:59145/amazoneco', fun
 });
 
 //Middleware
+app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+	resave: true,
+	saveUninitialized: true,
+	secret: secret.secretKey
+}));
+app.use(flash());
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
 
 
-app.post('/create-user', function (req, res, next) {
-	var user = new User();
-	
-	user.profile.name = req.body.name;
-	user.password = req.body.password;
-	user.email = req.body.email;
-	
-	user.save(function (err) {
-		if (err) return next(err);
-		
-		res.json('Succesfully created a new user');
-		
-	});
-});
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
+app.use(mainRoutes);
+app.use(userRoutes);
 
 
-app.listen(3000, function(err) {
+
+
+app.listen(secret.port, function(err) {
 	if (err) throw err;
-	console.log("Server is Running on port 3000");
+	console.log("Server is Running on port " + secret.port);
 });
